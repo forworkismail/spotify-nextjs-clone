d0b88e5a06c1a9791168257ec2fc41cf6e62e12e
Ismail <forwork.ismail@gmail.com>
2022-04-26 22:38:01 +0430

    Create main layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,16 +1,21 @@
-
 import { Box } from '@chakra-ui/layout';
 import React from 'react';
 
-interface Props {
-
-}
+interface Props {}
 const PlayerLayout: React.FC<Props> = ({ children }) => {
   return (
-    <Box>
-      Layout
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        Sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="250px">
+        {children}
+      </Box>
+      <Box position="absolute" bottom="0" left="0">
+        player
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default PlayerLayout
+export default PlayerLayout;
