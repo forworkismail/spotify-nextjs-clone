1f2d9373ed66f91bfcba4a5f2a53ade62a9a3642
Ismail <forwork.ismail@gmail.com>
2022-04-28 21:05:49 +0430

    Create list of menu items in sidebar

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,14 +1,14 @@
 import { Box } from '@chakra-ui/layout';
-import React from 'react';
+import Sidebar from './sidebar';
 
 interface Props {}
 const PlayerLayout: React.FC<Props> = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        Sidebar
+        <Sidebar />
       </Box>
-      <Box marginLeft="250px" marginBottom="250px">
+      <Box marginLeft="250px" marginBottom="100px">
         {children}
       </Box>
       <Box position="absolute" bottom="0" left="0">
